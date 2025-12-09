554e6b56ef5a974699e857d2d410c3b58bbd3d4e
nchenche <nicolas.chevrollier@laposte.net>
2025-12-09 11:18:05 +0100

    update footer

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="p-4 mt-auto">
-      <p className="text-sm text-center">© 2024 My App</p>
+      <p className="text-sm text-center">© 2025 PEP-EDIT - RPBS Platform</p>
     </footer>
   );
 }
