f4fceb71415b3543d9c43ab496713af1de4321cc
nchenche <nicolas.chevrollier@laposte.net>
2025-12-11 18:29:00 +0100

    remove unused constraints toggle button

--- frontend/src/components/peptide-editor/ChainComponent/ChainsToolbar.jsx
+++ frontend/src/components/peptide-editor/ChainComponent/ChainsToolbar.jsx
@@ -55,7 +55,7 @@ export default function ChainsToolbar({
             </ButtonGroup>
 
             {/* Bonds: Link / Unlink */}
-            <ButtonGroup size="small" variant="outlined" sx={{ '& .MuiButton-root': btnSx }}>
+            {/* <ButtonGroup size="small" variant="outlined" sx={{ '& .MuiButton-root': btnSx }}>
                 <Tooltip title="Link residues (2D sketch)" arrow>
                     <Button
                         onClick={onToggleConstraintsMode}
@@ -67,7 +67,7 @@ export default function ChainsToolbar({
                         Constraints
                     </Button>
                 </Tooltip>
-            </ButtonGroup>
+            </ButtonGroup> */}
         </Box>
     );
 }
